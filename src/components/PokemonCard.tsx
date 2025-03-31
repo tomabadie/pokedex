b75bc0acb2a9759d490441a86ef52f473326304d
tomabadie <abadie.thomas@gmail.com>
2025-03-31 23:39:16 +0200

    Add array pokemonList - Update PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,21 @@
-function PokemonCard() {
-    return <figure> 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+];
+  
+const PokemonCard = () => {
+    const pokemon = pokemonList[1];
+    return (
+        <figure> 
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p> }
+            <figcaption>{pokemon.name}</figcaption> 
+        </figure>
+    )
 }
 
 export default PokemonCard;
