33c1ed386041e8f1971cc95d70ab2233b69cd0d5
tomabadie <abadie.thomas@gmail.com>
2025-04-02 00:05:37 +0200

    Update pokemonList - Update nav accordingly using .map method

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,23 @@ import PokemonCard from "./components/PokemonCard";
 const pokemonList = [
   {
     name: "bulbasaur",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "mew",
@@ -29,9 +45,18 @@ function App() {
 
   return (
     <div>
+      <nav>
+        {pokemonList.map(poke => (
+          <button 
+            type="button" 
+            key={poke.name} 
+            onClick={() => setPokemonName(poke.name)}
+          >
+            {poke.name}
+          </button>
+        ))}
+      </nav>
       <PokemonCard pokemon={pokemon} />
-      <button type="button" onClick={() => setPokemonName("bulbasaur")}> Bulbasaur </button>
-      <button type="button" onClick={() => setPokemonName("mew")}> Mew </button>
     </div>
   );
 }
